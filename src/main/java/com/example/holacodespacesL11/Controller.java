17a45c82f76d60831a9a5b8f5c2b1b60f9b43310
Ernesto Anaya <ernestex@gmail.com>
2023-06-26 17:44:16 +0000

    enviando proyecto java

---
+++ src/main/java/com/example/holacodespacesL11/Controller.java
@@ -0,0 +1,17 @@
+package com.example.holacodespacesL11;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+public class Controller{
+
+    @GetMapping(path="/")
+    public String home(){
+        return "FEANAYA - Ernesto Anaya Ruiz";
+    }
+
+}
